8f4d838dfe80241a1ca9357d2c3981c913e9d7c4
Otis <thanhnd15032002@gmail.com>
2025-04-14 11:57:58 +0700

    define list stock api

--- src/modules/stock/stock.controller.ts
+++ src/modules/stock/stock.controller.ts
@@ -1,4 +1,36 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpStatus, Query, Res } from '@nestjs/common';
+import { StockService } from './stock.service';
+import { Response } from 'express';
 
 @Controller('stock')
-export class StockController {}
+export class StockController {
+  constructor(private readonly stockServices: StockService) {}
+
+  @Get('list')
+  async gets(@Query() query: Record<string, any>, @Res() res: Response) {
+    try {
+      const {
+        orderBy = 'id',
+        orderDirection = 'DESC',
+        page = 1,
+        pageSize = 10,
+        ...whereCondition
+      } = query;
+      const stocks = await this.stockServices.gets({
+        whereCondition,
+        orderBy,
+        orderDirection: orderDirection.toUpperCase(),
+        page,
+        pageSize,
+      });
+      return res.status(HttpStatus.OK).json({ success: true, ...stocks });
+    } catch (error) {
+      if (!res.headersSent) {
+        return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+          message: error.message,
+          success: false,
+        });
+      }
+    }
+  }
+}
--- src/modules/stock/stock.service.ts
+++ src/modules/stock/stock.service.ts
@@ -1,4 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { StockRepository } from './repositories/stock.repository';
 
 @Injectable()
-export class StockService {}
+export class StockService {
+  constructor(private readonly stockRepository: StockRepository) {}
+  async gets(query: {
+    orderBy: string;
+    orderDirection: 'ASC' | 'DESC';
+    page: number;
+    pageSize: number;
+    whereCondition: Record<string, any>;
+  }): Promise<any> {
+    try {
+      const { page, pageSize, orderBy, orderDirection, whereCondition } = query;
+      let numberPage = pageSize;
+      const queryBuilder = this.stockRepository.createQueryBuilder('stock');
+      if (orderBy) {
+        queryBuilder.orderBy(`stock.${orderBy}`, orderDirection || 'ASC');
+      }
+      queryBuilder.take(numberPage).skip((page - 1) * numberPage);
+      const [data, totalCount] = await queryBuilder.getManyAndCount();
+      const totalPages = Math.ceil(totalCount / pageSize);
+      return {
+        data,
+        attrs: {
+          totalCount,
+          totalPages,
+          currentPage: Number(page),
+        },
+      };
+    } catch (error) {
+      throw error;
+    }
+  }
+}
