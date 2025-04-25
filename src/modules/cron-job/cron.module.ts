d96866ddd3ab13ef48e82ddb89d35f06e6823c7f
duy.nguyennhat <nnduy1999@gmail.com>
2025-04-25 14:43:41 +0700

    [Cron] fix error module

--- src/modules/cron-job/cron.module.ts
+++ src/modules/cron-job/cron.module.ts
@@ -1,22 +1,15 @@
-import { forwardRef, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { CronJob } from './cron';
-import { TokenModule } from '../token/token.module';
-import { AuthModule } from '../auth/auth.module';
-import { KakaoScheduleModule } from '../kakao-schedule/kakao-schedule.module';
-import { KakaoModule } from '../kakao/repositories/kakao.module';
-import { ProductModule } from '../product/product.module';
-import { AuctionModule } from '../auction/auction.module';
-import { StoreInventoryModule } from '../store-inventory/store-inventory.module';
 
 @Module({
   imports: [
-    forwardRef(() => TokenModule),
-    forwardRef(() => AuthModule),
-    forwardRef(() => KakaoScheduleModule),
-    forwardRef(() => KakaoModule),
-    forwardRef(() => ProductModule),
-    forwardRef(() => AuctionModule),
-    forwardRef(() => StoreInventoryModule),
+    // forwardRef(() => TokenModule),
+    // forwardRef(() => AuthModule),
+    // forwardRef(() => KakaoScheduleModule),
+    // forwardRef(() => KakaoModule),
+    // forwardRef(() => ProductModule),
+    // forwardRef(() => AuctionModule),
+    // forwardRef(() => StoreInventoryModule),
   ],
   providers: [CronJob],
   exports: [CronJob],
